0416cf74bef5e6c68eab573363a0c387e264b5ab
njpdevops <njpdevops@gmail.com>
2021-10-07 18:11:01 -0400

    fix: docs

--- pages/documentation/src/index.tsx
+++ pages/documentation/src/index.tsx
@@ -11,10 +11,21 @@ console.log(data)
 const imgUrl = new URL('./assets/600x600.png', import.meta.url).href
 
 const App = () => {
-    return <div>
-      <img src={imgUrl} />
+    return <div className="vh-100 d-flex flex-column overflow-hidden container">
+      <div className="d-flex flex-shrink-0 flex-row p-3 bg-primary">
+        <img src={imgUrl} className="me-3" style={{ height: 50, width: 50 }} />
+        <h1>Fn Repeats</h1>
+      </div>
+      <div className="d-flex flex-shrink-0 flex-row p-3" style={{ backgroundColor: "var(--bs-white)"}}>
+
       <p>Hello!</p>
+      </div>
     </div>
 }
 
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById("app"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("app")
+);
